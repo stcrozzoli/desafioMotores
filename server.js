c03d7bd94ce60a939f8447c1c800fb38e570c306
stcrozzoli <stcrozzoli@gmail.com>
2022-12-30 17:17:36 -0300

    antes del chat

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const { engine } = require('express-handlebars')
-app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.set('views', './views')
 app.set('view engine', 'handlebars')
@@ -13,19 +12,39 @@ let listaproductos = [{
     url: 'https://cdn2.iconfinder.com/data/icons/party-new-years/128/Party_Newyears_Disco_Lamp-64.png'
 }]
 
+
+// /////////////////////////
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+io.on('connection', socket => {
+    console.log('Un cliente se ha conectado');
+  });
+// ////////////////////////
+
+app.get('/productos', (req, res) => {
+    res.render('productos2', { listaproductos: listaproductos });
+});
+
+
 app.post('/productos', (req, res) => {
     listaproductos.push(req.body)
     res.redirect('/productos')
-})
-
-app.get('/productos', (req, res) => {
-    res.render('productos2', listaproductos)
+    console.log(listaproductos)
 })
 
 
 const PORT = 8080
-const server = app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${server.address().port}`)
-})
+// const server = app.listen(PORT, () => {
+//     console.log(`Servidor corriendo en el puerto ${server.address().port}`)
+// })
 
-server.on('error', error => console.log(`Error en servidor ${error}`))
+
+// /////////
+http.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+  });
+
+// server.on('error', error => console.log(`Error en servidor ${error}`))
+
+http.on('error', error => console.log(`Error en servidor ${error}`))
